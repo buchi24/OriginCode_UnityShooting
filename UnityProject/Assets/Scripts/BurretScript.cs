049765d39bc04279eca5cd244ee7b2a294cc4d8e
ryoctrl@gmail.com <ryoctrl@gmail.com>
2018-10-05 20:50:29 +0900

    移動/攻撃スクリプトを追加

---
+++ UnityProject/Assets/Scripts/BurretScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurretScript : MonoBehaviour {
+	private float speed = 1000f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.x += Time.deltaTime * speed;
+		transform.position = pos;
+		if(pos.x > Screen.width) {
+			Destroy(transform.gameObject);
+		}
+	}
+}
--- UnityProject/Assets/Scripts/CanvasScript.cs
+++ UnityProject/Assets/Scripts/CanvasScript.cs
@@ -7,6 +7,8 @@ public class CanvasScript : MonoBehaviour {
 	
 	// 画面幅
 	private int width = Screen.width;
+	// 画面高さ
+	private int height = Screen.height;
 	// 遠景画像Prefab
 	public GameObject enkeiPrefab;
 	// 遠景画像のインスタンス
@@ -15,9 +17,20 @@ public class CanvasScript : MonoBehaviour {
 	public GameObject kinkeiPrefab;
 	//近景画像インスタンス配列
 	private GameObject[] kinkeis = new GameObject[2];
+
+	//キャラクター画像prefab
+	public GameObject characterPrefab;
+	//private float[] positionRatios = {0.6875f, 0.75f, 0.8215f};
+	private float[] positionRatios = {0.6875f, 0.65f, 0.6215f};
+	private GameObject characterInstance;
+	int currentPos = -1;
+	float diff = 45f / 720f;
+	float posDiff = -1;
 	//背景を流すSpeed
 	//TODO: 外部設定変数に切り出す。
 	private float speed = 100;
+
+	public GameObject burretPrefab;
 
 	//
 	// ゲーム開始時に1度だけ呼ばれる関数（メソッド）
@@ -33,12 +46,37 @@ public class CanvasScript : MonoBehaviour {
 			kinkeis[i].transform.SetParent(transform, false);
 			x += width;
 		}
+
+		float charaY = (height / 2);
+		charaY -= height * positionRatios[0];
+	
+		
+		characterInstance = Instantiate(characterPrefab, new Vector3(characterPrefab.GetComponent<RectTransform>().sizeDelta.x / 2, charaY, 0), Quaternion.identity);
+		characterInstance.transform.SetParent(transform, false);
+
+		Vector2 charaSize = characterInstance.GetComponent<RectTransform>().sizeDelta;
+		charaSize.y = height / 3;
+		charaSize.x = charaSize.y / 4 * 5;
+		characterInstance.GetComponent<RectTransform>().sizeDelta = charaSize;
+
+		currentPos = 0;
+
+		posDiff = height * diff;
 	}
 	
 	///
 	/// 毎フレーム呼ばれる関数（メソッド)
 	///
 	void Update () {
+		updateBackground();
+		inputListen();
+		updateSpeed();
+	}
+
+	///
+	/// 背景を動かすメソッド。
+	///
+	private void updateBackground() {
 		for(int i = 0; i < kinkeis.Length; i++) {
 			Vector3 pos = kinkeis[i].transform.position;
 			GameObject otherObj = i == 0 ? kinkeis[1] : kinkeis[0];
@@ -46,5 +84,43 @@ public class CanvasScript : MonoBehaviour {
 			pos.x -= Time.deltaTime * speed;
 			kinkeis[i].transform.position = pos;
 		}
+	}
+		
+		
+	///
+	/// ユーザーからのキー入力を受け付けるメソッド
+	///
+	private void inputListen() {
+		if(Input.GetKeyDown(KeyCode.UpArrow)) move(false);
+		else if(Input.GetKeyDown(KeyCode.DownArrow)) move(true);
+		//攻撃キー入力
+		else if(Input.GetKeyDown(KeyCode.RightArrow)) attack();
+	}
+
+	private void attack() {
+		Vector3 pos = characterInstance.transform.position;
+		pos.y += 30f;
+		GameObject burretInstance = Instantiate(burretPrefab, pos, Quaternion.identity);
+		burretInstance.transform.SetParent(transform, true);
+
+	}
+
+	///
+	/// キャラクターの移動を司るメソッド。
+	///
+	private void move(bool isDown) {
+		Vector3 characterPos = characterInstance.transform.position;
+		if(!isDown && currentPos -1 >= 0) {
+			currentPos--;
+			characterPos.y += posDiff;
+		} else if(isDown && currentPos + 1 < positionRatios.Length) {
+			currentPos++;
+			characterPos.y -= posDiff;
+		}	
+		characterInstance.transform.position = characterPos;
+	}
+
+	private void updateSpeed() {
+
 	}
 }
